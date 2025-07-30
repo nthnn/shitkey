b441ec4956ca63ad51177fed1be6b638dd815fcb
Nathanne Isip <nathanneisip@gmail.com>
2025-07-30 11:30:27 +0800

    Constants and declarations

---
+++ consts.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025 - Nathanne Isip
+ * This file is part of Shitkey.
+ *
+ * Shitkey is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Shitkey is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shitkey. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main
+
+const (
+	saltSize  = 32
+	keySize   = 32
+	nonceSize = 12
+	skFileExt = ".sk"
+	version   = "shitkey v1.4"
+)
